a4a258055166faa575c2f0acd2987dc3cd0360fe
Brandon Chung <brandon.chung@devmounta.in>
2018-08-21 10:45:57 -0500

    initial commit on this repo, added ghpages and changes to server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { json } = require('body-parser');
+const path = require('path');
 const peopleCtrl = require('./controllers/peopleCtrl');
 
 const app = express();
 
 app.use(json());
+app.use(express.static(path.join(__dirname, '../build')))
 
 //testing endpoint
 app.get('/api/test', (req, res) => {
@@ -18,7 +20,11 @@ app.post('/api/people', peopleCtrl.createPerson);
 app.delete('/api/people/:id', peopleCtrl.deletePerson);
 app.put('/api/people/:id', peopleCtrl.updatePerson);
 
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
